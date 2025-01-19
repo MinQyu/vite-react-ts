ae8d13ce68ed5ba1e118c7b2dfb1ea823e90d8de
Min-gyu <jorushs@gmail.com>
2025-01-19 15:31:47 +0900

    [추가] prose 컴포넌트 생성

---
+++ src/components/prose.tsx
@@ -0,0 +1,17 @@
+function Prose() {
+  return (
+    <div className="prose">
+      <header>
+        <h1 lang="en">Custom Vite Template</h1>
+      </header>
+      <main>
+        <p>React 프로젝트 시작을 위한 사용자 정의 템플릿</p>
+      </main>
+      <footer>
+        <p>MinQyu</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Prose;
